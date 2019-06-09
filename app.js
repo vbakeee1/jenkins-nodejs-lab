b2bec81ea51060ac9d689fdb965d6ce40477da96
prabhat pankaj <prabhatiitbhu@gmail.com>
2019-06-09 22:08:57 +0530

    ne changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('odejs for github ci cd');
+  res.send('Nodejs for github ci cd');
 });
 
 app.listen(process.env.PORT || 5000);
