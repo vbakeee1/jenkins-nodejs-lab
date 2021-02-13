ea47be0e42d418e27828dd065088f3eed2a58143
vbakeee1 <74676410+vbakeee1@users.noreply.github.com>
2021-02-12 19:57:43 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Nodejs for github TesT');
+  res.send('Nodejs for github test');
 });
 
 app.listen(process.env.PORT || 5000);
